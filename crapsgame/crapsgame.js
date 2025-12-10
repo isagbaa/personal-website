1fdb00320c6211640fdaa9d422c5879a458455a2
Isagba gerare <gerardisagba17@gmail.com>
2025-12-10 17:01:45 +0100

    added regex username validation

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -2,12 +2,20 @@
  const crapsgameregistration = "craps-game-registration";
  const crapsgamemainsection = "craps-game-main-section";
  function registercrapsplayer(){
-   let crapsusername =   document.getElementById(crapsgameinputid).value;
-   alert("Welcome to the Craps Game, " + crapsusername + "!");
-   removeregistrationpane();
-   showmaingamesection();
+   let crapsusername =   document.getElementById(crapsgameinputid).value
+   
+  
+  
+   if (/^[0-9]/.test(crapsusername) ||  /^[_.]/.test(crapsusername) || crapsusername.length <= 5 || /\s/.test(crapsusername) || /[^a-zA-Z0-9_.]/.test(crapsusername)) {
+    alert("username must be more than 5 characters long, alphanumeric with underscore and dot only, cannot start with a digit or special symbol, no spaces allowed");
     }
+    else{
+       removeregistrationpane()
+        showmaingamesection()
+   }
+   
 
+  }
     function removeregistrationpane(){
         document.getElementById(crapsgameregistration).style.display="none"
     }
